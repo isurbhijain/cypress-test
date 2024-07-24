1de55b2760ca7ace04cb78e13657b086b6a5767f
PriyansuPanda <priyansu.panda@globallogic.com>
2024-07-24 15:49:50 +0530

    Changes to spec file.

--- cypress/e2e/AmazonTesting/amazone2eTesting.cy.js
+++ cypress/e2e/AmazonTesting/amazone2eTesting.cy.js
@@ -1,12 +1,7 @@
 import desiredProductPage from "../../locators/Page Locators/desiredProductPage.cy"
 import homePage from "../../locators/Page Locators/homePage.cy"
 import searchResultPage from "../../locators/Page Locators/searchResultPage.cy"
-import signinPage from "../../locators/Page Locators/signinPage.cy"
 
-//const homepage = new homePage()
-//const signinpage = new signinPage()
-//const searchresultpage = new searchResultPage()
-//const desiredproductpage = new desiredProductPage()
 describe("Amazon Testing",function(){
     before(function(){
         cy.fixture("dataAmazon.json").then(function(data){
